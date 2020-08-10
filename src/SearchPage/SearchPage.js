78b9c7da398bc51de11af9e2d6a6f53f32623e2a
warrioryoko <miangpellegri@gmail.com>
2020-08-10 10:22:19 -0700

    Added SearchPage, refactored event handlers. Have to figure out event on submit button.

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import request from 'superagent';
 import PokeItem from './PokeItem.js';
+import PokeList from './PokeList.js';
 
 // props are how parent components talk to child components -- we "pass" props from parents to children
 class SearchPage extends React.Component {
@@ -8,38 +9,117 @@ class SearchPage extends React.Component {
         search: '',
         searchBy: 'pokemon',
         isLoading: false,
-        pokeState: []
+        pokeState: [],
+        currentPage: 1,
+        totalPages: 1
 }
 
-handleClick = async () => {
-    // 4) go hit the api and get the data
-    this.setState({ isLoading: true })
-    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=1000&${this.state.searchBy}=${this.state.search}`);
+componentDidMount = async () => {
+    const params = new URLSearchParams(this.props.location.search);
 
-    this.setState({ 
+    const searchBy = params.get('searchBy');
+    const page = params.get('page');
+    const search = params.get('search');
+
+    if (searchBy && page && search) {
+        await this.setState({
+            searchBy: searchBy,
+            currentPage: page,
+            search: search
+        });
+    }
+
+    await this.makeRequest()
+}
+
+makeRequest = async () => {
+    this.setState({ isLoading: true });
+
+    const data = await request.get(`https://alchemy-pokedex.herokuapp..com/api/pokedex?page=${this.state.currentPage}&perPage=20&${this.state.searchBy}=${this.state.search}`);
+
+    await this.setState({
         pokeState: data.body.results,
+        totalPages: Math.ceil(data.body.count / 20),
         isLoading: false,
     })
+
+    const params = new URLSearchParams(this.props.location.search);
+
+    params.set('search', this.state.search);
+    params.set('searchBy', this.state.searchBy);
+    params.set('page', this.state.currentPage);
+
+    this.props.history.push('?' + params.toString())
 }
 
+handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await this.setState({
+        currentPage: 1
+    })
+    await this.makeRequest()
+}
+
+handleNextClick = async () => {
+    await this.setState({ currentPage: Number(this.state.currentPage) + 1 });
+
+    await this.makeRequest();
+}
+
+handlePrevClick = async () => {
+    await this.setState({ currentPage: Number(this.state.currentPage) - 1 });
+
+    await this.makeRequest();
+}
+
+// handleClick = async () => {
+//     // 4) go hit the api and get the data
+//     this.setState({ isLoading: true })
+//     const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=1000&${this.state.searchBy}=${this.state.search}`);
+
+//     this.setState({ 
+//         pokeState: data.body.results,
+//         isLoading: false,
+//     })
+// }
+
 render() {
-    const { isLoading, pokeState } = this.state;
+    const { 
+        isLoading, 
+        pokeState,
+        currentPage,
+        totalPages,
+    } = this.state;
     
     return (
-        <div>
-            <input onChange={(e) => this.setState({ search: e.target.value})} />
-            <select onChange={(e) => { this.setState({ searchBy: e.target.value })} }>
-                <option value='pokemon'>name</option>
-                <option value='type'>type</option>
-                <option value='attack'>attack</option>
-                <option value='defense'>defense</option>
-            </select>
-            <button onClick={this.handleClick}>Fetch Pokemon!</button>
-            {
-                isLoading 
-                ? <p>LOADING</p> 
-                : pokeState.map(poke => <PokeItem pokemon={poke} />)
-            }
+        <div className="search">
+            <div className="sidebar">
+                <form onSubmit={this.handleSubmit}>
+                <input onChange={(e) => this.setState({ search: e.target.value})} value={this.state.search}/>
+                <select onChange={(e) => { this.setState({ searchBy: e.target.value })} } value={this.state.searchBy}>
+                    <option value='pokemon'>name</option>
+                    <option value='type'>type</option>
+                    <option value='attack'>attack</option>
+                    <option value='defense'>defense</option>
+                </select>
+                <button onClick={this.handleClick}>Fetch Pokemon!</button>
+                </form>
+            </div>
+            <div className="results">
+                {
+                    isLoading 
+                    ? <p>LOADING</p> 
+                    // : pokeState.map(poke => <PokeItem pokemon={poke} 
+                    : <PokeList
+                            handleNextClick={this.handleNextClick}
+                            handlePrevClick={this.handlePrevClick}
+                            currentPage={currentPage}
+                            pokeState={pokeState}
+                            totalPages={totalPages}
+                        />
+                }
+            </div>
         </div>
     );
     }
